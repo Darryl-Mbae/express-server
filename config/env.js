0ab88c7777e37652e9445bed622d600a3bb5fbea
Darryl Mbae <102142446+Darryl-Mbae@users.noreply.github.com>
2025-03-21 17:52:42 +0300

    Update env.js

--- config/env.js
+++ config/env.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const {PORT, NODE_ENV} = process.env;
+export const {PORT, NODE_ENV, REDIS_URL, OPTIMIZE_API_KEY, ARCJET_KEY, ARCJET_ENV} = process.env;
